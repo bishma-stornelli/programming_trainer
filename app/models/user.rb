120b2b64f2b9df0554199b4be77a14db44359511
Bishma Stornelli <bishma.stornelli@gmail.com>
2013-02-28 18:40:35 -0430

    Agregada funcionalidad de bcrypt-ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :date_of_birth, :email, :firstname, :lastname, :password_digest
+  attr_accessible :date_of_birth, :email, :firstname, :lastname, :password, :password_confirmation
+  
+  has_secure_password
 end
