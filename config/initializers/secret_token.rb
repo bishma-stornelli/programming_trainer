01f473df2848fbb3a5205de09dd6193faa1e48b5
Bishma Stornelli <bishma.stornelli@gmail.com>
2013-02-28 18:11:16 -0430

    Agregada estructura inicial del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProgrammingTrainer::Application.config.secret_token = 'ffca1658e29c42e8f5f7cc7a89b61b7a4330e92a02f5c9a4facb88057688e53eb3dfc0b434dccbb7141be573b9960caa9c7e446990badfcb56e3ce3c542c2cd9'
