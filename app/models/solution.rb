0418a80e2dcbb50a85d83cbb5efbececb495464b
Bishma Stornelli <bishma.stornelli@gmail.com>
2013-02-28 19:29:54 -0430

    Agregadas validaciones en solution y test_case

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -2,4 +2,6 @@ class Solution < ActiveRecord::Base
   belongs_to :user
   belongs_to :problem
   attr_accessible :date, :status, :time_running, :file
+  
+  validates :user, :problem, :date , :status, :file, :presence => true
 end
--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -1,4 +1,7 @@
 class TestCase < ActiveRecord::Base
   belongs_to :problem
   attr_accessible :input, :max_time, :output, :title
+  
+  validates :problem, :title, :max_time, :input, :output, :presence => true
+  validates :title, :uniqueness => {:scope => :problem_id}
 end
