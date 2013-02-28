d1db8fc299145167c55f96773795d488e2a9010d
Bishma Stornelli <bishma.stornelli@gmail.com>
2013-02-28 18:43:03 -0430

    Agregadas validaciones al modelo User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :date_of_birth, :email, :firstname, :lastname, :password, :password_confirmation
   
   has_secure_password
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :firstname, :lastname, :presence => true, :format => { :with => /[a-zA-Z ]*/ }
 end
