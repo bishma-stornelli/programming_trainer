bd8959cb8ee67221f7c4afb1bd70bf76bf729737
Bishma Stornelli <bishma.stornelli@gmail.com>
2013-02-28 19:31:56 -0430

    Agregada has_attached_file en solution y test_case

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -3,5 +3,7 @@ class Solution < ActiveRecord::Base
   belongs_to :problem
   attr_accessible :date, :status, :time_running, :file
   
+  has_attached_file :file
+  
   validates :user, :problem, :date , :status, :file, :presence => true
 end
--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -2,6 +2,9 @@ class TestCase < ActiveRecord::Base
   belongs_to :problem
   attr_accessible :input, :max_time, :output, :title
   
+  has_attached_file :input
+  has_attached_file :output
+  
   validates :problem, :title, :max_time, :input, :output, :presence => true
   validates :title, :uniqueness => {:scope => :problem_id}
 end
